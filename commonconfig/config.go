7ce67c7eba66091b879836a71f9449994cec61fc
random life <ligong19@yeah.net>
2021-05-01 18:53:07 +0800

    添加特征

--- commonconfig/config.go
+++ commonconfig/config.go
@@ -9,16 +9,27 @@ type HttpServerConfig struct {
 
 type ServerConfig struct {
 	HttpServerConfig `toml:",inline"`
-	GRPCPort         int    `toml:"grpc_port"`
-	HTTPSPort        int    `toml:"https_port"`
-	ProjectName      string `toml:"project_name"`
-	PromePort        int    `toml:"prome_port"`
-	PProfPort        int    `toml:"pprof_port"`
-	Debug            bool   `toml:"debug"`
+	ServiceEnv       map[string]string `toml:"sevice_env"`
+	GRPCPort         int               `toml:"grpc_port"`
+	HTTPSPort        int               `toml:"https_port"`
+	ProjectName      string            `toml:"project_name"`
+	PromePort        int               `toml:"prome_port"`
+	PProfPort        int               `toml:"pprof_port"`
+	Debug            bool              `toml:"debug"`
 }
 
 type MongoClientConfig struct {
 	URL         string `toml:"url"`
 	MaxPoolSize int    `toml:"max_pool_size"`
 	Timeout     int    `toml:"timeout"`
+}
+
+type RedisConfig struct {
+	Address            string         `toml:"address"`
+	Password           string         `toml:"password"`
+	DefaultDB          int            `toml:"default_db"`
+	DBIndexes          map[string]int `toml:"db_indexes"`
+	MinIdleConnections int            `toml:"min_idel_conns"`
+	ReadTimeout        int            `toml:"read_timeout"`
+	DialTimout         int            `toml:"dial_timeout"`
 }

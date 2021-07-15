5ca48be375da3153be3e5031b97516cb3a56b8c8
random life <ligong19@yeah.net>
2021-07-15 08:17:54 +0800

    add grpc pool

--- commonconfig/config.go
+++ commonconfig/config.go
@@ -1,52 +1,61 @@
 package commonconfig
 
 type HttpServerConfig struct {
-	HTTPPort       int `toml:"http_port"`
-	ReadTimeout    int `toml:"read_timeout"`
-	WriteTimeout   int `toml:"write_timeout"`
-	MaxHeaderBytes int `toml:"max_header_bytes"`
+	HTTPPort       int `json:"http_port" toml:"http_port"`
+	ReadTimeout    int `json:"read_timeout" toml:"read_timeout"`
+	WriteTimeout   int `json:"write_timeout" toml:"write_timeout"`
+	MaxHeaderBytes int `json:"max_header_bytes" toml:"max_header_bytes"`
 }
 
 type ServerConfig struct {
-	HttpServerConfig `toml:",inline"`
-	ServiceEnv       map[string]string `toml:"sevice_env"`
-	GRPCPort         int               `toml:"grpc_port"`
-	HTTPSPort        int               `toml:"https_port"`
-	ProjectName      string            `toml:"project_name"`
-	PromePort        int               `toml:"prome_port"`
-	PProfPort        int               `toml:"pprof_port"`
-	Debug            bool              `toml:"debug"`
+	HttpServerConfig `json:",inline" toml:",inline"`
+	ServiceEnv       map[string]string `json:"sevice_env" toml:"sevice_env"`
+	GRPCPort         int               `json:"grpc_port" toml:"grpc_port"`
+	HTTPSPort        int               `json:"https_port" toml:"https_port"`
+	ProjectName      string            `json:"project_name" toml:"project_name"`
+	PromePort        int               `json:"prome_port" toml:"prome_port"`
+	PProfPort        int               `json:"pprof_port" toml:"pprof_port"`
+	Debug            bool              `json:"debug" toml:"debug"`
 }
 
 type MongoClientConfig struct {
-	URL         string `toml:"url"`
-	MaxPoolSize int    `toml:"max_pool_size"`
-	Timeout     int    `toml:"timeout"`
+	URL         string `json:"url" toml:"url"`
+	MaxPoolSize int    `json:"max_pool_size" toml:"max_pool_size"`
+	Timeout     int    `json:"timeout" toml:"timeout"`
 }
 
 type RedisConfig struct {
-	Address            string         `toml:"address"`
-	Password           string         `toml:"password"`
-	DefaultDB          int            `toml:"default_db"`
-	DBIndexes          map[string]int `toml:"db_indexes"`
-	MinIdleConnections int            `toml:"min_idel_conns"`
-	ReadTimeout        int            `toml:"read_timeout"`
-	DialTimout         int            `toml:"dial_timeout"`
+	Address            string         `json:"address" toml:"address"`
+	Password           string         `json:"password" toml:"password"`
+	DefaultDB          int            `json:"default_db" toml:"default_db"`
+	DBIndexes          map[string]int `json:"db_indexes" toml:"db_indexes"`
+	MinIdleConnections int            `json:"min_idel_conns" toml:"min_idel_conns"`
+	ReadTimeout        int            `json:"read_timeout" toml:"read_timeout"`
+	DialTimout         int            `json:"dial_timeout" toml:"dial_timeout"`
 }
 
 type FinderConfig struct {
-	Type      string `toml:"type"`
-	Timeout   int    `toml:"timeout"`
-	Endpoint  string `toml:"endpoint"`
-	Region    string `toml:"region"`
-	AccessKey string `toml:"access_key"`
-	SecretKey string `toml:"secret_key"`
+	Type      string `json:"type" toml:"type"`
+	Timeout   int    `json:"timeout" toml:"timeout"`
+	Endpoint  string `json:"endpoint" toml:"endpoint"`
+	Region    string `json:"region" toml:"region"`
+	AccessKey string `json:"access_key" toml:"access_key"`
+	SecretKey string `json:"secret_key" toml:"secret_key"`
 }
 
 type DownloaderConfig struct {
-	FinderConfig `toml:",inline"`
-	SourcePath   string            `toml:"source_path"`
-	LocalPath    string            `toml:"local_path"`
-	Interval     int               `toml:"interval"`
-	Extra        map[string]string `toml:"extra"`
+	FinderConfig `json:",inline" toml:",inline"`
+	SourcePath   string            `json:"source_path" toml:"source_path"`
+	LocalPath    string            `json:"local_path" toml:"local_path"`
+	Interval     int               `json:"interval" toml:"interval"`
+	Extra        map[string]string `json:"extra" toml:"extra"`
+}
+
+type GRPCClientConfig struct {
+	Url            string `json:"url" toml:"url"`
+	DialTimeout    int    `json:"dail_timeout" toml:"dail_timeout"`
+	RequestTimeout int    `json:"request_timeout" toml:"request_timeout"`
+	InitConn       int    `json:"init_conn" toml:"init_conn"`
+	MaxConn        int    `json:"max_conn" toml:"max_conn"`
+	HealthCheck    bool   `json:"health_check" toml:"health_check"`
 }
---
+++ grpc_pool/pool.go
@@ -0,0 +1,111 @@
+package grpc_pool
+
+import (
+	"context"
+	"fmt"
+	"github.com/uopensail/ulib/commonconfig"
+	"golang.org/x/exp/rand"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health/grpc_health_v1"
+	"strings"
+	"time"
+)
+
+type grpcConn struct {
+	conn    *grpc.ClientConn
+	status  bool
+	url     string
+	timeout int
+}
+
+type Pool struct {
+	Config  *commonconfig.GRPCClientConfig
+	Clients []*grpcConn
+	Count   int
+}
+
+func check(conns []*grpcConn) {
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		<-ticker.C
+		for i := 0; i < len(conns); i++ {
+			if conns[i].status {
+				health, err := grpc_health_v1.NewHealthClient(conns[i].conn).Check(context.Background(),
+					&grpc_health_v1.HealthCheckRequest{Service: ""})
+				if err != nil || health.Status != grpc_health_v1.HealthCheckResponse_SERVING {
+					conns[i].status = false
+				}
+			} else {
+				timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Microsecond*time.Duration(conns[i].timeout))
+				conn, err := grpc.DialContext(timeoutCtx,
+					conns[i].url,
+					grpc.WithDefaultServiceConfig(`{loadBalancingConfig:[{"round_robin":{}}]}`),
+					grpc.WithInsecure(),
+					grpc.WithBlock(),
+				)
+				if err != nil {
+					conns[i].status = false
+				} else {
+					conns[i].conn, conns[i].status = conn, true
+				}
+				cancel()
+			}
+
+		}
+	}
+}
+
+func NewPool(cfg *commonconfig.GRPCClientConfig) *Pool {
+	if !strings.HasPrefix(cfg.Url, "grpc:///") {
+		panic(fmt.Errorf("prefix error: %s", cfg.Url))
+	}
+	urls := strings.Split(cfg.Url[8:], ",")
+	maxConn := 3
+	if cfg.MaxConn > 0 {
+		maxConn = cfg.MaxConn
+	}
+
+	timeout := 30
+	if cfg.DialTimeout > 0 {
+		timeout = cfg.DialTimeout
+	}
+	pool := &Pool{
+		Config:  cfg,
+		Clients: make([]*grpcConn, maxConn*len(urls)),
+		Count:   maxConn * len(urls),
+	}
+
+	for i := 0; i < len(urls); i++ {
+		for j := 0; j < maxConn; j++ {
+			timeoutCtx, cancel := context.WithTimeout(context.Background(), time.Microsecond*time.Duration(timeout))
+			conn, err := grpc.DialContext(timeoutCtx,
+				urls[i],
+				grpc.WithDefaultServiceConfig(`{loadBalancingConfig:[{"round_robin":{}}]}`),
+				grpc.WithInsecure(),
+				grpc.WithBlock(),
+			)
+			if err != nil {
+				pool.Clients[i*maxConn+j].status = false
+			} else {
+				pool.Clients[i*maxConn+j].conn, pool.Clients[i*maxConn+j].status = conn, true
+			}
+			cancel()
+		}
+	}
+	if cfg.HealthCheck {
+		go check(pool.Clients)
+	}
+	return pool
+}
+
+func (pool *Pool) GetConn() *grpc.ClientConn {
+	index := rand.Intn(pool.Count)
+	tmpIndex := 0
+	for j := 0; j < pool.Count; j++ {
+		tmpIndex = (index + j) % pool.Count
+		if pool.Clients[tmpIndex].status {
+			return pool.Clients[tmpIndex].conn
+		}
+	}
+	return nil
+}

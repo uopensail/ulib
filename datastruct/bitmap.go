781d36c60e7c0838b34aa62a8fe48d92f59a2aa0
kungjim <7629149+kungjim@users.noreply.github.com>
2021-07-24 20:06:25 +0800

    add bitmap datastruct

---
+++ datastruct/bitmap.go
@@ -0,0 +1,51 @@
+package datastruct
+
+type BitMapStatus int8
+
+const (
+	NoMarkBitMapStatus BitMapStatus = 0
+	TrueBitMapStatus   BitMapStatus = 1
+	FalseBitMapStatus  BitMapStatus = 2
+	ErrorBitMapStatus  BitMapStatus = 3
+)
+
+type DoubleBitMap []byte
+
+func CreateDoubleBitMap(size int) BitMap {
+	bitmap := make(BitMap, ((size>>3)+1)<<1)
+	return bitmap
+}
+func (bitmap DoubleBitMap) Mark(index int, bv bool) {
+	byteIndex := index >> 2
+	offset := (index & 3) << 1
+	if bv {
+		bitmap[byteIndex] |= (1 << offset)
+	} else {
+		bitmap[byteIndex] |= (2 << offset)
+	}
+}
+
+func (bitmap DoubleBitMap) Check(index int) BitMapStatus {
+	byteIndex := index >> 2
+	offset := (index & 3) << 1
+	return BitMapStatus((bitmap[byteIndex] >> offset) & 3)
+}
+
+type BitMap []byte
+
+func CreateBitMap(size int) BitMap {
+	bitmap := make(BitMap, (size>>3)+1)
+	return bitmap
+}
+
+func (bitmap BitMap) MarkTrue(index int) {
+	byteIndex := index >> 3
+	offset := index & 7
+	bitmap[byteIndex] |= (1 << offset)
+}
+
+func (bitmap BitMap) Check(index int) bool {
+	byteIndex := index >> 3
+	offset := index & 7
+	return !(bitmap[byteIndex]&(1<<offset) == 0)
+}

0b79010178152f3260a3dfa036e53e8475c1a259
kungjim <7629149+kungjim@users.noreply.github.com>
2021-08-06 21:40:07 +0800

    add sample.util

--- sample/utils.go
+++ sample/utils.go
@@ -1,5 +1,9 @@
 package sample
 
+import (
+	"github.com/uopensail/ulib/utils"
+)
+
 func GetFloat32ByKey(features *Features, key string) float32 {
 	feature, ok := features.Feature[key]
 	if !ok {
@@ -66,4 +70,44 @@ func GetInt64ListByKey(features *Features, key string) []int64 {
 		return nil
 	}
 	return feature.GetInt64List().GetValue()
+}
+
+func MakeInt64List(vs []int64) *Feature {
+	return &Feature{Kind: &Feature_Int64List{Int64List: &Int64List{Value: vs}}}
+}
+
+func MakeStringList(vs []string) *Feature {
+	byteVs := make([][]byte, len(vs))
+	for i := 0; i < len(vs); i++ {
+		byteVs[i] = []byte(vs[i])
+	}
+	return &Feature{Kind: &Feature_BytesList{BytesList: &BytesList{Value: byteVs}}}
+}
+
+func MakeFloat32List(vs []float32) *Feature {
+	return &Feature{Kind: &Feature_FloatList{FloatList: &FloatList{Value: vs}}}
+}
+
+func MakeFloat32ListWithStrs(ss []string) *Feature {
+	vs := make([]float32, len(ss))
+	for i := 0; i < len(ss); i++ {
+		vs[i] = utils.String2Float32(ss[i])
+	}
+	return &Feature{Kind: &Feature_FloatList{FloatList: &FloatList{Value: vs}}}
+}
+
+func MakeByteListWithStrs(ss []string) *Feature {
+	vs := make([][]byte, len(ss))
+	for i := 0; i < len(ss); i++ {
+		vs[i] = []byte(ss[i])
+	}
+	return &Feature{Kind: &Feature_BytesList{BytesList: &BytesList{Value: vs}}}
+}
+
+func MakeInt64ListWithStrs(ss []string) *Feature {
+	vs := make([]int64, len(ss))
+	for i := 0; i < len(ss); i++ {
+		vs[i] = utils.String2Int64(ss[i])
+	}
+	return &Feature{Kind: &Feature_Int64List{Int64List: &Int64List{Value: vs}}}
 }
